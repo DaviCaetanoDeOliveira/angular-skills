e70b8fe45a9339171e25cd7b0da2ac2bbdf6f6f8
DaviCaetanoDeOliveira <131567096+DaviCaetanoDeOliveira@users.noreply.github.com>
2023-08-17 00:02:56 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatCardModule, MatChipsModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule, MatTooltipModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -11,11 +11,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { FakeApiService } from './fake-api.service';
 import { HelloComponent } from './hello.component';
 
-
-
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule, MatChipsModule, MatCardModule, HttpClientModule, InMemoryWebApiModule.forRoot(FakeApiService, {delay: 5000}) ],
-  declarations: [ AppComponent, HelloComponent, DashboardComponent, CardComponent ],
-  bootstrap:    [ AppComponent ]
+	imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatProgressSpinnerModule, MatButtonModule, MatIconModule, MatChipsModule, MatCardModule, MatTooltipModule, HttpClientModule, InMemoryWebApiModule.forRoot(FakeApiService, {delay: 5000}) ],
+	declarations: [ AppComponent, HelloComponent, DashboardComponent, CardComponent ],
+	bootstrap:    [ AppComponent ],
+	schemas:      [ CUSTOM_ELEMENTS_SCHEMA ],
 })
-export class AppModule { }
+
+export class AppModule 
+{ }
